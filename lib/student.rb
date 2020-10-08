85534889160cb65c6dc52bac5c4ee3008f13c9c3
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-08 17:41:36 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -95,10 +95,10 @@ class Student
       SELECT *
       FROM students 
       WHERE grade = 10 
-      LIMIT x
+      LIMIT ?
     SQL
     
-    DB[:conn].execute(sql).map do |row|
+    DB[:conn].execute(sql, x).map do |row|
       self.new_from_db(row)
     end
   end

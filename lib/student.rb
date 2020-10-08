bac5c8cb1ee33e6cbeb2c8d447155bf3784f955e
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-08 17:32:09 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -85,9 +85,10 @@ class Student
       
     SQL
  
-    DB[:conn].execute(sql).map do |row|
-      self.new_from_db(row)
-    end
+    DB[:conn].execute(sql)
+    #DB[:conn].execute(sql).map do |row|
+     # self.new_from_db(row)
+   # end
   end
   
   def self.first_x_students_in_grade_10

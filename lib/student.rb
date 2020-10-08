a09550baefc1b59bd15d0bf23390eb19011f2c72
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-08 17:30:48 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -90,4 +90,14 @@ class Student
     end
   end
   
+  def self.first_x_students_in_grade_10
+    sql = <<-SQL
+      SELECT *
+      FROM students 
+      WHERE grade = 10 
+      LIMIT 1 
+    SQL
+    DB[:conn].execute(sql)
+  end
+  
 end

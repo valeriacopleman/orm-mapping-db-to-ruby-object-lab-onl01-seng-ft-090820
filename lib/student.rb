9a38d6fcd82f9d7b6ced99d1f6902d5921f21435
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-08 17:43:12 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -115,7 +115,15 @@ class Student
     end.first
   end
   
-  def self.all_students_in_grade_X
+  def self.all_students_in_grade_X(x)
+    sql = <<-SQL
+      SELECT *
+      FROM students 
+      WHERE grade = ?
+    SQL
     
+    DB[:conn].execute(sql, x).map do |row|
+      self.new_from_db(row)
+    end
   end
 end

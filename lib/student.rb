5c6b23427910494b01bf44d3ef3b24fd1e941f5f
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-08 17:35:28 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -90,6 +90,10 @@ class Student
     end
   end
   
+  def self.first_x_students_in_grade_10
+    
+  end
+  
   def self.first_student_in_grade_10
     sql = <<-SQL
       SELECT *
@@ -102,4 +106,7 @@ class Student
     end.first
   end
   
+  def self.all_students_in_grade_X
+    
+  end
 end

0b8ea075e7bd486ad4a3950e2812a39845a44b8c
Valeria  Copleman <valeriacopleman@gmail.com>
2020-10-08 17:31:23 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -97,7 +97,9 @@ class Student
       WHERE grade = 10 
       LIMIT 1 
     SQL
-    DB[:conn].execute(sql)
+    DB[:conn].execute(sql).map do |row|
+      self.new_from_db(row)
+    end
   end
   
 end
